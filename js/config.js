862e3ca73be83d63f80476defb86f25d0dbcfc16
Pierre-Luc Dion <pdion@voilevacance.ca>
2015-02-15 10:16:19 -0500

    wadagame customization

--- js/config.js
+++ js/config.js
@@ -9,9 +9,9 @@
  */
 
 /* Configs */
-var indexName = "name"; //Ex: twitter
-var docType = "type"; //Ex: tweet
+var indexName = "wadagame"; //Ex: twitter
+var docType = "files"; //Ex: tweet
 var maxResultsSize = 10;
 var host = "localhost"; //Ex: http://ec2-123-aws.com
 var port = 9200;
-var protocol = ""; //Default: same as browser
+var protocol = "http"; //Default: same as browser
